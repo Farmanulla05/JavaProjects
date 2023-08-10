9c8698563e808de0cc4c0c83ba6d932a57530806
Farmanulla05 <141896062+Farmanulla05@users.noreply.github.com>
2023-08-10 10:19:51 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+package javacore;
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("hello farman");
+int age = 22;
+int elegable = 18;
+System.out.println(age > elegable);
+System.out.println(age<= elegable);
+	}
+
+}
